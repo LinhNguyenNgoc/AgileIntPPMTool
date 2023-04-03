df3c27aaff4b58f54c55fe84d4063c3efb2dff59
LinhNguyenNgoc <75782178+LinhNguyenNgoc@users.noreply.github.com>
2023-04-03 20:42:07 +0700

    first commit

---
+++ PPMToolFullStackReactJava/src/main/java/io/agileintelligence/ppmtool/PpmToolFullStackReactJavaApplication.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackReactJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolFullStackReactJavaApplication.class, args);
+    }
+
+}
